b0c930496f8f471f975efb286ec8e857979e9620
Killers0992 <Killers0992@gmail.com>
2021-04-06 19:45:58 +0200

    New stuff

---
+++ SCP_ET.API/Classes/KillTrigger.cs
@@ -0,0 +1,34 @@
+﻿using SCP_ET.API.Enums;
+using SCP_ET.API.Extensions;
+using SCP_ET.API.Interfaces;
+using SCP_ET.API.Interfaces.EventsArgs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCP_ET.API.Classes
+{
+    public abstract class KillTrigger : IEntity
+    {
+        public event PluginSystem.CustomEventHandler<EntityTakeDamageEvent> EntityTakeDamage;
+        public event PluginSystem.CustomEventHandler<EntityKillEvent> EntityKill;
+        public void OnEntityTakeDamage(EntityTakeDamageEvent ev) => EntityTakeDamage.InvokeSafely(ev);
+        public void OnEntityKill(EntityKillEvent ev) => EntityKill.InvokeSafely(ev);
+
+        public abstract DimensionType CurrentDimension { get; set; }
+        public static List<KillTrigger> KillTriggers { get; set; } = new List<KillTrigger>();
+        public abstract bool IsPlayer { get; }
+        public abstract bool IsScp { get; }
+        public abstract bool IsNpc { get; }
+        public abstract bool IsValidTarget { get; }
+        public abstract object GameObject { get; }
+        public abstract void TakeDamage(float dmgAmount, IEntity damager, DeathTypes deathType);
+        public abstract void KillEntity();
+        public abstract void KillEntity(DeathTypes deathType, IEntity killer);
+        public abstract void OnDestroy();
+        public abstract Vector Position { get; set; }
+        public abstract Vector Rotation { get; set; }
+    }
+}
